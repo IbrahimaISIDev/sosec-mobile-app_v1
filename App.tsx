e0700fcf7eeafecb9057b49e8d037d3b4ed59aa5
IbrahimaISIDev <sorydiallo371@gmail.com>
2025-03-28 14:34:25 +0000

    Update App.tsx file, package.json and package-lock.json

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; // Si tu utilises Expo, sinon retire cette ligne
+import { AuthProvider } from './src/contexts/AuthContext';
+import { SyncProvider } from './src/contexts/SyncContext';
+import RootNavigator from './src/navigation/RootNavigator';
 
-const App: React.FC = () => {
+const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello, Expo with TypeScript!</Text>
-    </View>
+    <AuthProvider>
+      <SyncProvider>
+        <RootNavigator />
+        <StatusBar style="auto" /> {/* Optionnel, pour Expo */}
+      </SyncProvider>
+    </AuthProvider>
   );
 };
 
