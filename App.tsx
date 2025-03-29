6c9033200343af74df3ee55aa9f890872fbcd8db
IbrahimaISIDev <sorydiallo371@gmail.com>
2025-03-29 10:57:59 +0000

    Ajout des dossiers et fichiers pour la configuration avec Android et IOS pour Firebase

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import RootNavigator from "./src/navigation/RootNavigator";
 import { ActivityIndicator, View } from "react-native";
 import NetInfo from "@react-native-community/netinfo";
 import { syncQueue } from "./src/queue/queue";
+import { OfflineProvider } from "./src/contexts/OfflineContext";
 
 const SyncHandler: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { token } = useAuth();
@@ -28,12 +29,12 @@ const SyncHandler: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 const App = () => {
   return (
     <AuthProvider>
-      <SyncProvider>
-        <SyncHandler>
+      <OfflineProvider>
+        <SyncProvider>
           <RootNavigator />
           <StatusBar style="auto" />
-        </SyncHandler>
-      </SyncProvider>
+        </SyncProvider>
+      </OfflineProvider>
     </AuthProvider>
   );
 };
